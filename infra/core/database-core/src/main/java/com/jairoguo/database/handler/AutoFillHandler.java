1aa3fb5b97100a9fa3240cb34f1cc1176a8eca97
Jairo Guo <jairoguo@163.com>
2022-05-25 22:12:10 +0800

    feat: 添加数据库填充处理

---
+++ infra/core/database-core/src/main/java/com/jairoguo/database/handler/AutoFillHandler.java
@@ -0,0 +1,29 @@
+package com.jairoguo.database.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Jairo Guo
+ */
+@Component
+public class AutoFillHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", LocalDateTime.now(), metaObject);
+        this.setFieldValByName("updateTime", LocalDateTime.now(), metaObject);
+        this.setFieldValByName("createUser", "System", metaObject);
+        this.setFieldValByName("updateUser", "System", metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", LocalDateTime.now(), metaObject);
+        // TODO: 更新数据填充数据修改人
+
+
+    }
+}
