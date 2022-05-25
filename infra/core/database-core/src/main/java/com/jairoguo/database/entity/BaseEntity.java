4aafbf80207c99ee1caadf683e149bc0ca68945b
Jairo Guo <jairoguo@163.com>
2022-05-25 23:45:42 +0800

    feat: 添加数据库实体通用属性

---
+++ infra/core/database-core/src/main/java/com/jairoguo/database/entity/BaseEntity.java
@@ -0,0 +1,89 @@
+package com.jairoguo.database.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author Jairo Guo
+ */
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    @Column(length = 20)
+    private Long id;
+
+    private boolean deleted;
+
+    @TableField(fill = FieldFill.INSERT)
+    protected LocalDateTime createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    protected LocalDateTime updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    @Column(length = 30)
+    protected String createUser;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @Column(length = 30)
+    protected String updateUser;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+}
