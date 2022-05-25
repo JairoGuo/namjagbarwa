6cb3b435c27cece83ee6ee4d39bcf3087c7fc354
Jairo Guo <jairoguo@163.com>
2022-05-26 01:10:19 +0800

    feat: 添加Redis Key生成工具

---
+++ infra/core/redis-core/src/main/java/com/jairoguo/redis/util/RedisKey.java
@@ -0,0 +1,60 @@
+package com.jairoguo.redis.util;
+
+import com.jairoguo.exception.redis.RedisKeyException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+import java.util.Optional;
+
+/**
+ * @author Jairo Guo
+ */
+@Component
+public class RedisKey {
+    private String projectName;
+    @Value("${spring.application.name}")
+    private String moduleName;
+
+    public String getKey(String... fields) {
+        fields = Optional.ofNullable(fields).orElseThrow(() -> {
+            throw new RedisKeyException("fields不能为null");
+        });
+        if (fields.length == 0) throw new RedisKeyException("fields内容不能为空");
+
+        int index = 0;
+        StringBuilder param = new StringBuilder();
+        for (String field : fields) {
+            field = Optional.ofNullable(field).orElseThrow(() -> {
+                throw new RedisKeyException("fields中的选项不能为null");
+            });
+            if (field.isEmpty()) throw new RedisKeyException("fields中的选项不能为空字符串");
+            if (param.isEmpty()) param.append(field);
+            else {
+                if (index % 2 == 1) param.append(":").append(field);
+                else param.append(".").append(field);
+            }
+            index++;
+        }
+
+        /*
+            [{project_name}]:{modules}:{{business_uid}[:{uid_value}][.{{business_uid2}[:{uid_value2}]]
+         */
+        if (Optional.ofNullable(projectName).isPresent()) {
+            return MessageFormat.format("{0}:{1}:{2}", projectName, moduleName, param).toUpperCase();
+        } else {
+            return MessageFormat.format("{0}:{1}", moduleName, param).toUpperCase();
+        }
+    }
+    public void setAttributes(String projectName) {
+        if (Optional.ofNullable(projectName).isPresent()) {
+            this.projectName = projectName;
+        }
+    }
+    public void setAttributes(String projectName, String moduleName) {
+        this.setAttributes(projectName);
+        if (Optional.ofNullable(moduleName).isPresent()) {
+            this.moduleName = moduleName;
+        }
+    }
+}
