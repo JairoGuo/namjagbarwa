73c77a9883972dca4f658fcee75bc2fb91c017c7
Jairo Guo <jairoguo@163.com>
2022-05-29 22:58:44 +0800

    feat: 领域服务
    1. 添加创建账户
    2. 添加验证账户

---
+++ modules/account/src/main/java/com/jairoguo/account/domain/service/AccountDomainService.java
@@ -0,0 +1,19 @@
+package com.jairoguo.account.domain.service;
+
+import com.jairoguo.account.domain.model.aggregate.Account;
+import com.jairoguo.common.base.DomainService;
+
+/**
+ * @author Jairo Guo
+ */
+public interface AccountDomainService extends DomainService {
+    /**
+     * 创建账户
+     *
+     * @param account 账户聚合根
+     * @return 布尔值
+     */
+    Account createAccount(Account account);
+
+    Account verify(Account account);
+}
