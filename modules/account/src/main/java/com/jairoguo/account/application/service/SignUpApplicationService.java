3b882e41944d763b622aa8325dbf688be1b544a1
Jairo Guo <jairoguo@163.com>
2022-05-29 23:00:16 +0800

    feat: 账户应用服务

---
+++ modules/account/src/main/java/com/jairoguo/account/application/service/SignUpApplicationService.java
@@ -0,0 +1,27 @@
+package com.jairoguo.account.application.service;
+
+import com.jairoguo.account.application.bo.SignUpBO;
+import com.jairoguo.account.domain.model.aggregate.Account;
+import com.jairoguo.account.domain.service.AccountDomainService;
+import com.jairoguo.common.base.ApplicationService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Jairo Guo
+ */
+@Service
+public class SignUpApplicationService implements ApplicationService {
+
+    @Resource
+    private AccountDomainService accountDomainService;
+
+    @Transactional(rollbackFor = Exception.class)
+    public Account register(SignUpBO signUpBO) {
+
+        return accountDomainService.createAccount(signUpBO.account());
+    }
+
+}
