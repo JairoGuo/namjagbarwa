8074d98b207dcf0e65ba934af6198c09c6942ffa
Jairo Guo <jairoguo@163.com>
2022-05-29 23:00:16 +0800

    feat: 账户应用服务

---
+++ modules/account/src/main/java/com/jairoguo/account/application/bo/SignInBO.java
@@ -0,0 +1,11 @@
+package com.jairoguo.account.application.bo;
+
+import com.jairoguo.account.domain.model.aggregate.Account;
+
+/**
+ * @author Jairo Guo
+ */
+public record SignInBO(
+        Account account
+) {
+}
