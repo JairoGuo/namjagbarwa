435400e330d42e135c524bed16004b29d262df90
Jairo Guo <jairoguo@163.com>
2022-05-31 11:57:18 +0800

    feat: 添加Spring Cloud支持

--- modules/sms/src/main/java/com/jairoguo/sms/SmsApplication.java
+++ modules/sms/src/main/java/com/jairoguo/sms/SmsApplication.java
@@ -2,7 +2,9 @@ package com.jairoguo.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SmsApplication {
 
