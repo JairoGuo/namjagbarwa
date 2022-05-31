e3f19c88dc5b1d04ce2e609836ecb84ef73e3009
Jairo Guo <jairoguo@163.com>
2022-05-31 22:33:47 +0800

    feat: 添加路由过滤器

---
+++ gateway/src/main/java/com/jairoguo/gateway/config/GlobalFilterConfigure.java
@@ -0,0 +1,40 @@
+package com.jairoguo.gateway.config;
+
+import cn.dev33.satoken.reactor.filter.SaReactorFilter;
+import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpUtil;
+import com.jairoguo.common.result.Result;
+import com.jairoguo.common.result.ResultCodeEnum;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jairo Guo
+ */
+@Configuration
+public class GlobalFilterConfigure {
+    @Bean
+    public SaReactorFilter getFilter() {
+
+        return new SaReactorFilter()
+                // 拦截路由
+                .addInclude("/**")
+                // 放行账户服务路由
+                .addExclude(
+                        "/account/signUp/email",
+                        "/account/signUp/phone",
+                        "/account/signIn/password",
+                        "/account/signIn/smsCode"
+                )
+                // 放行sms服务路由
+                .addExclude(
+                        "/sms/code/get"
+                )
+                // 认证函数: 每次请求执行
+                .setAuth(obj -> SaRouter.match("/account/getAccount", StpUtil::checkLogin))
+                // 异常处理函数：每次认证函数发生异常时执行此函数
+                .setError(e -> Result.info(ResultCodeEnum.INFO, "未登录"))
+                .setBeforeAuth(r -> {
+                });
+    }
+}
