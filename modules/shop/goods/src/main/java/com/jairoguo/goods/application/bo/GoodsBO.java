93133d798927a3acd97c50219577d597c15f2b62
Jairo Guo <jairoguo@163.com>
2022-06-27 19:53:44 +0800

    feat: 商品应用服务层

---
+++ modules/shop/goods/src/main/java/com/jairoguo/goods/application/bo/GoodsBO.java
@@ -0,0 +1,13 @@
+package com.jairoguo.goods.application.bo;
+
+import com.jairoguo.goods.domain.model.aggregate.Goods;
+import lombok.Builder;
+
+/**
+ * @author Jairo Guo
+ */
+
+@Builder
+public record GoodsBO(
+        Goods goods) {
+}
