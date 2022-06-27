a0d06e77fd7bc0071c39b78f17f6a4ba44ec37cc
Jairo Guo <jairoguo@163.com>
2022-06-27 19:51:54 +0800

    feat: 仓储接口

--- modules/shop/goods/src/main/java/com/jairoguo/goods/domain/repository/GoodsRepository.java
+++ modules/shop/goods/src/main/java/com/jairoguo/goods/domain/repository/GoodsRepository.java
@@ -5,10 +5,14 @@ import com.jairoguo.common.base.Repository;
 import com.jairoguo.goods.domain.model.aggregate.Goods;
 import com.jairoguo.goods.domain.model.entity.id.GoodsNumber;
 
+import java.util.List;
+
 /**
  * 商品仓储
  *
  * @author Jairo Guo
  */
 public interface GoodsRepository extends Repository<Goods, GoodsNumber> {
+
+    List<Goods> list();
 }
