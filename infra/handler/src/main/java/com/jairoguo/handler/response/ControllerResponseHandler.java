784ff134e979660cce25b14eafdfd1e2177ca64e
Jairo Guo <jairoguo@163.com>
2022-06-29 10:30:46 +0800

    feat: 封装统一返回处理

---
+++ infra/handler/src/main/java/com/jairoguo/handler/response/ControllerResponseHandler.java
@@ -0,0 +1,38 @@
+package com.jairoguo.handler.response;
+
+import com.jairoguo.common.result.Result;
+import com.jairoguo.common.result.ResultBody;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author Jairo Guo
+ */
+
+@RestControllerAdvice
+public class ControllerResponseHandler implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return !returnType.getParameterType().isAssignableFrom(ResultBody.class);
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+
+        if (body instanceof String) {
+            return body;
+        }
+
+        return Result.success(body);
+    }
+}
