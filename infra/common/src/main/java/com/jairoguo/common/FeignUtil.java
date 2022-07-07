67c8f6537230386b2c8e6afd3c5eab0111129cd7
Jairo Guo <jairoguo@163.com>
2022-07-07 22:28:01 +0800

    feat: Feign工具类

---
+++ infra/common/src/main/java/com/jairoguo/common/FeignUtil.java
@@ -0,0 +1,12 @@
+package com.jairoguo.common;
+
+/**
+ * @author Jairo Guo
+ */
+public class FeignUtil {
+
+    public static final String FEIGN_ID = "feign_id";
+
+    private FeignUtil() {
+    }
+}
