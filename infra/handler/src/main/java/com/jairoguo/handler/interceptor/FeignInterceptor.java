5cbc89f81ecef29bec2efb52e3cbddd8d94608f9
Jairo Guo <jairoguo@163.com>
2022-07-31 15:32:36 +0800

    feat: 传递请求头

--- infra/handler/src/main/java/com/jairoguo/handler/interceptor/FeignInterceptor.java
+++ infra/handler/src/main/java/com/jairoguo/handler/interceptor/FeignInterceptor.java
@@ -6,7 +6,10 @@ import feign.RequestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 /**
@@ -20,12 +23,24 @@ public class FeignInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
 
-        log.info("FeignInterceptor FeignUtil.FEIGN_ID(feign_id) 是否存在: {}", requestTemplate.headers().get(FeignUtil.FEIGN_ID) == null);
+        log.info("FeignInterceptor FeignUtil.FEIGN_ID(feign_id): {}", requestTemplate.headers().get(FeignUtil.FEIGN_ID));
         if (requestTemplate.headers().get(FeignUtil.FEIGN_ID) == null) {
             requestTemplate.header(FeignUtil.FEIGN_ID, String.valueOf(UUID.randomUUID()));
         }
-        log.info("FeignInterceptor FeignUtil.FEIGN_ID(feign_id): {}", requestTemplate.headers().get(FeignUtil.FEIGN_ID));
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes != null ? requestAttributes.getRequest() : null;
+        /* TODO: 部分头参数影响系统运行
+        Enumeration<String> headerNames = request.getHeaderNames();
+        if (Objects.nonNull(headerNames)) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                requestTemplate.header(name, request.getHeader(name));
+            }
+        }
+        */
+        if (request != null) {
+            requestTemplate.header("cookie", request.getHeader("cookie"));
 
-
+        }
     }
 }
